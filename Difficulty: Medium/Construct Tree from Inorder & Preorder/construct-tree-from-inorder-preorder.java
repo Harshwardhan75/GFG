e22d60ad0cee4e2883cdd90d524343833c731fbb
Harshwardhan Shambharkar <149169405+Harshwardhan75@users.noreply.github.com>
2025-02-18 21:27:58 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Construct Tree from Inorder & Preorder/construct-tree-from-inorder-preorder.java
@@ -0,0 +1,91 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Node {
+    int data;
+    Node left, right;
+
+    Node(int key) {
+        data = key;
+        left = right = null;
+    }
+}
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+
+        while (t-- > 0) {
+            Node root = null;
+
+            // Read inorder array
+            String[] inorderStr = (br.readLine()).trim().split(" ");
+            int inorder[] = new int[inorderStr.length];
+            for (int i = 0; i < inorderStr.length; i++) {
+                inorder[i] = Integer.parseInt(inorderStr[i]);
+            }
+
+            // Read preorder array
+            String[] preorderStr = (br.readLine()).trim().split(" ");
+            int preorder[] = new int[preorderStr.length];
+            for (int i = 0; i < preorderStr.length; i++) {
+                preorder[i] = Integer.parseInt(preorderStr[i]);
+            }
+
+            Solution ob = new Solution();
+            root = ob.buildTree(inorder, preorder);
+            postOrder(root);
+            System.out.println();
+        }
+    }
+
+    // Function to print postorder traversal of the tree
+    public static void postOrder(Node root) {
+        if (root == null) return;
+
+        postOrder(root.left);
+        postOrder(root.right);
+        System.out.print(root.data + " ");
+    }
+}
+
+// } Driver Code Ends
+
+
+
+
+/*
+class Node {
+    int data;
+    Node left, right;
+
+    Node(int key) {
+        data = key;
+        left = right = null;
+    }
+}
+*/
+
+class Solution {
+    public static Node buildTree(int inorder[], int preorder[]) {
+        // code here
+        int[] index=new int[]{0};
+        Map<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<inorder.length;i++){
+            map.put(inorder[i],i);
+        }
+        return construct(preorder,index,0,preorder.length-1,map);
+    }
+    
+    static Node construct(int[] preorder,int[] index,int start,int end,Map<Integer,Integer> map){
+        if(start>end)   return null;
+    
+        Node root=new Node(preorder[index[0]++]);
+        int i=map.get(root.data);
+        root.left=construct(preorder,index,start,i-1,map);
+        root.right=construct(preorder,index,i+1,end,map);
+        return root;
+    }
+}
