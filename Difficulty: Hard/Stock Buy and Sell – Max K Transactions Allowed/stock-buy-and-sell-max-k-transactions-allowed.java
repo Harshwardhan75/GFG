8e2cc0d11b6d132763e672e66bf78e498c5c7ba5
Harshwardhan Shambharkar <149169405+Harshwardhan75@users.noreply.github.com>
2025-03-19 16:30:40 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Stock Buy and Sell – Max K Transactions Allowed/stock-buy-and-sell-max-k-transactions-allowed.java
+++ Difficulty: Hard/Stock Buy and Sell – Max K Transactions Allowed/stock-buy-and-sell-max-k-transactions-allowed.java
@@ -27,33 +27,37 @@ public class Main {
 
 
 class Solution {
-    //No
-    static int f(int ind, int buy, int arr[], int k, int dp[][][]){
-        if(k<0)return 0;
-        if(ind==arr.length)return 0;
+    static int max = 0;
+    static int maxProfit(int prices[], int k) {
+        int n=prices.length;
+        int[][][] dp=new int[n][k][2];
+        max= k;
+        for(int[][] i: dp)  for(int[] j: i) Arrays.fill(j,-1);
+
+        return solve(prices,0,1,0,dp);
+    }
+    
+    static int solve(int[] prices,int ind,int buy,int day,int[][][] dp){
+        if(ind==prices.length)
+            return 0;
         
-        if(dp[ind][buy][k]!=-1)return dp[ind][buy][k];
+        if(day>=max)  return 0;
         
-        int profit=0;
+        if(dp[ind][day][buy]!=-1)    return dp[ind][day][buy];
+
+        int profit = 0;
         if(buy==1){
-            profit=Math.max(-arr[ind]+f(ind+1, 0,arr, k-1,dp), f(ind+1, 1, arr, k,dp));
-        }else {
-            profit=Math.max(arr[ind]+f(ind+1, 1,arr, k,dp), f(ind+1, 0, arr, k,dp));
+            int take = -prices[ind]+solve(prices,ind+1,0,day,dp);
+            int nottake = solve(prices,ind+1,1,day,dp);
+            profit=Math.max(take,nottake);
         }
-        
-        return dp[ind][buy][k]=profit;
+        else{
+            int take = prices[ind]+solve(prices,ind+1,1,day+1,dp);
+            int nottake = solve(prices,ind+1,0,day ,dp);
+            profit=Math.max(take,nottake);
+        }
+
+        return dp[ind][day][buy]=profit;
     }
     
-    
-    
-    static int maxProfit(int arr[], int k) {
-        int dp[][][]=new int[arr.length][2][k+1];
-        for(int a[][]: dp){
-            for(int b[]: a){
-                Arrays.fill(b,-1);
-            }
-        }
-        return f(0, 1, arr, k,dp);
-        
-    }
 }
