ed9a2f302cd30a95facef7757e8a0bb2c66c8e42
Harshwardhan Shambharkar <149169405+Harshwardhan75@users.noreply.github.com>
2025-03-27 21:28:47 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Boolean Parenthesization/boolean-parenthesization.java
+++ Difficulty: Hard/Boolean Parenthesization/boolean-parenthesization.java
@@ -25,53 +25,63 @@ class GFG {
 class Solution {
     static int countWays(String s) {
         // code here
-        int n = s.length();
-
-        // Create DP tables to store the number of ways to evaluate to true and false
-        int[][] trueWays = new int[n][n];
-        int[][] falseWays = new int[n][n];
-
-        // Initialize the DP tables for single characters
-        for (int i = 0; i < n; i++) {
-            if (s.charAt(i) == 'T') {
-                trueWays[i][i] = 1;
-                falseWays[i][i] = 0;
-            } else if (s.charAt(i) == 'F') {
-                trueWays[i][i] = 0;
-                falseWays[i][i] = 1;
-            }
+        int n=s.length();
+        int[][][] dp=new int[n][n][2];
+        for(int[][] i: dp)  for(int[] j: i) Arrays.fill(j,-1);
+        return solve(s.toCharArray(),0,n-1,1,dp);
+    }
+    
+    static int solve(char[] s,int i,int j,int isTrue,int[][][] dp){
+        if(i>j)
+            return 0;
+        
+        if(i==j){
+            if(isTrue==1)
+                return s[i]=='T'?1:0;
+            else
+                return s[i]=='F'?1:0;
         }
-
-        // Fill the DP tables for substrings of length > 1
-        for (int length = 3; length <= n; length += 2) {
-            for (int i = 0; i <= n - length; i++) {
-                int j = i + length - 1;
-
-                for (int k = i + 1; k < j; k += 2) {
-                    char operator = s.charAt(k);
-
-                    int leftTrue = trueWays[i][k - 1];
-                    int leftFalse = falseWays[i][k - 1];
-                    int rightTrue = trueWays[k + 1][j];
-                    int rightFalse = falseWays[k + 1][j];
-
-                    // Combine results based on the operator
-                    if (operator == '&') {
-                        trueWays[i][j] += leftTrue * rightTrue;
-                        falseWays[i][j] += leftTrue * rightFalse + leftFalse * rightTrue + leftFalse * rightFalse;
-                    } else if (operator == '|') {
-                        trueWays[i][j] += leftTrue * rightTrue + leftTrue * rightFalse + leftFalse * rightTrue;
-                        falseWays[i][j] += leftFalse * rightFalse;
-                    } else if (operator == '^') {
-                        trueWays[i][j] += leftTrue * rightFalse + leftFalse * rightTrue;
-                        falseWays[i][j] += leftTrue * rightTrue + leftFalse * rightFalse;
-                    }
+        
+        if(dp[i][j][isTrue]!=-1)
+            return dp[i][j][isTrue];
+        
+        int ways = 0;
+        
+        for(int ind=i+1;ind<=j-1;ind+=2){
+            int lt=solve(s,i,ind-1,1,dp);
+            int lf=solve(s,i,ind-1,0,dp);
+            int rt=solve(s,ind+1,j,1,dp);
+            int rf=solve(s,ind+1,j,0,dp);
+                
+            if(s[ind]=='&'){
+                if(isTrue==1){
+                    ways+=lt*rt;
+                }
+                else{
+                    ways+=(lt*rf)+(lf*rt)+(lf*rf);
+                }
+            }
+            else if(s[ind]=='|'){
+                if(isTrue==1){
+                    ways+=(lt*rt)+(lt*rf)+(lf*rt);
+                }
+                else{
+                    ways+=(lf*rf);
+                }
+            }
+            else{
+                if(isTrue==1){
+                    ways+=(lt*rf)+(lf*rt);
+                }
+                else{
+                    ways+=(lt*rt)+(lf*rf);
                 }
             }
         }
-
-        // Return the number of ways to evaluate the whole expression to true
-        return trueWays[0][n - 1];
-
+        return dp[i][j][isTrue] = ways;
+    }
+    
+    static int add(int a,int b){
+        return a+b;
     }
 }
