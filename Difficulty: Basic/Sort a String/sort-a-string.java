cdc25e2d6914dab7e641b498d3f1ae34b24283de
Harshwardhan Shambharkar <149169405+Harshwardhan75@users.noreply.github.com>
2025-05-24 23:09:45 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Sort a String/sort-a-string.java
@@ -0,0 +1,21 @@
+// User function Template for Java
+class Solution {
+    String sort(String s) {
+        // code here
+        // int[] freq=new int[26];
+        // for(char c: s.toCharArray())
+        //     freq[c-'a']++;
+        
+        // StringBuilder sb=new StringBuilder();
+        
+        // for(int i=0;i<26;i++){
+        //     while(freq[i]>0){
+        //         sb.append((char)(i+'a'));
+        //         freq[i]--;
+        //     }
+        // }
+        
+        // return sb.toString();
+        char[] c=s.toCharA
+    }
+}
