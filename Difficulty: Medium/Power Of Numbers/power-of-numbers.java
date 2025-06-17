00b92080dc8fff9a70f29910ca339107004ec143
Harshwardhan Shambharkar <149169405+Harshwardhan75@users.noreply.github.com>
2025-06-17 23:16:37 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Power Of Numbers/power-of-numbers.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int reverseExponentiation(int n) {
+        // code here
+        if(n==10)
+            return 10;
+        
+        return exponent(n,n);
+    }
+    
+    int exponent(int base,int power){
+        while(power>0){
+            if(power%2)
+        }
+    }
+}
