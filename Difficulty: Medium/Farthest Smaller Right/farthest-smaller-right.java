792f7fda8688eb78c67e959f1ba6164dd99ac840
Harshwardhan Shambharkar <149169405+Harshwardhan75@users.noreply.github.com>
2025-08-19 18:23:28 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Farthest Smaller Right/farthest-smaller-right.java
+++ Difficulty: Medium/Farthest Smaller Right/farthest-smaller-right.java
@@ -24,6 +24,7 @@ class Solution {
         }
         
         int query(int index,int low,int high,int value){
+                
             if(low==high){
                 return seg[index][1];
             }
