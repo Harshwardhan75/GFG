151bf31ebd73c96e339f81b70e8ef5266a363483
Harshwardhan Shambharkar <149169405+Harshwardhan75@users.noreply.github.com>
2025-08-27 00:19:13 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Count the number of possible triangles/count-the-number-of-possible-triangles.java
+++ Difficulty: Medium/Count the number of possible triangles/count-the-number-of-possible-triangles.java
@@ -1,52 +1,33 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.util.*;
-
-class Geeks {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine());
-        for (int g = 0; g < t; g++) {
-            String[] str = (br.readLine()).trim().split(" ");
-            int arr[] = new int[str.length];
-            for (int i = 0; i < str.length; i++) {
-                arr[i] = Integer.parseInt(str[i]);
-            }
-            System.out.println(new Solution().countTriangles(arr));
-            System.out.println("~");
-        }
-    }
-}
-
-// } Driver Code Ends
-
-
-
-
-// User function Template for Java
-
 class Solution {
-    // Function to count the number of possible triangles.
-    static int countTriangles(int arr[]) {
+    public int countTriangles(int arr[]) {
         // code here
         Arrays.sort(arr);
-        int n=arr.length;
-        int count=0;
         
+        int n = arr.length;
+        int count = 0;
         
-        for(int i=n-1;i>=2;i--){
-            int j=0,k=i-1;
-            while(j<k){
-                if(arr[j]+arr[k]>arr[i]){
-                    count+=k-j;
-                    k--;
-                }
-                else
-                    j++;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                int index = find(arr,j+1,n-1,arr[i]+arr[j]);
+                count+= index-(j+1);
             }
         }
         
         return count;
     }
-
+    
+    int find(int[] arr,int low,int high,int sum){
+        while(low<=high){
+            int mid = (low+high)>>1;
+            
+            if(arr[mid]>=sum){
+                high = mid - 1;
+            }
+            else{
+                low = mid + 1;
+            }
+        }
+        
+        return low;
+    }
 }
