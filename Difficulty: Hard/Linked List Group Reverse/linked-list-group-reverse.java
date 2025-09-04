6d51f3d7adefaf936f3885ebe809476fc794bb47
Harshwardhan Shambharkar <149169405+Harshwardhan75@users.noreply.github.com>
2025-09-04 12:36:50 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Linked List Group Reverse/linked-list-group-reverse.java
+++ Difficulty: Hard/Linked List Group Reverse/linked-list-group-reverse.java
@@ -1,65 +1,4 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.lang.*;
-import java.util.*;
-
-class Node {
-    int data;
-    Node next;
-
-    Node(int key) {
-        data = key;
-        next = null;
-    }
-}
-
-class ReverseInSize {
-    static Node head;
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-        PrintWriter out = new PrintWriter(System.out);
-
-        int t = Integer.parseInt(in.readLine().trim());
-
-        while (t-- > 0) {
-
-            String s[] = in.readLine().trim().split(" ");
-            int a1 = Integer.parseInt(s[0]);
-            Node head = new Node(a1);
-            Node tail = head;
-            for (int i = 1; i < s.length; i++) {
-                int a = Integer.parseInt(s[i]);
-                // addToTheLast(new Node(a));
-                tail.next = new Node(a);
-                tail = tail.next;
-            }
-
-            int k = Integer.parseInt(in.readLine().trim());
-            Solution ob = new Solution();
-            Node res = ob.reverseKGroup(head, k);
-            printList(res, out);
-            out.println();
-
-            out.println("~");
-        }
-        out.close();
-    }
-
-    public static void printList(Node node, PrintWriter out) {
-        while (node != null) {
-            out.print(node.data + " ");
-            node = node.next;
-        }
-    }
-}
-
-// } Driver Code Ends
-
-
-
-
-/*node class of the linked list
+/*
 class Node
 {
     int data;
@@ -70,53 +9,52 @@ class Node
         next = null;
     }
 }
-
 */
 
 class Solution {
-    public static Node reverseKGroup(Node head, int kk) {
+    public Node reverseKGroup(Node head, int k) {
         // code here
-        Node temp=head;
-        Node prevNode=null;
+        Node prevLast=null;
+        Node temp = head;
+        
         while(temp!=null){
-            Node k=find(temp,kk);
+            Node KthNode = findKthNode(temp,k);
+            Node nextNode = KthNode!=null?KthNode.next:null;
+            if(KthNode!=null)
+                KthNode.next = null;
+                
+            KthNode = reverse(temp);
             
-            Node nextNode=k.next;
-            k.next=null;
-            k=reverse(temp);
-            
-            if(temp==head)
-                head=k;
-            else
-                prevNode.next=k;
-            
-            prevNode=temp;
-            temp=nextNode;
+            if(temp==head){
+                head = KthNode;
+            }
+            else{
+                prevLast.next = KthNode;
+            }
+            prevLast = temp;
+            temp = nextNode;
         }
         
         return head;
     }
     
-    static Node find(Node temp,int k){
-        while(temp.next!=null){
-            k--;
-            if(k==0)
-                break;
-            temp=temp.next;
+    Node findKthNode(Node node,int k){
+        while(k-->1 && node!=null){
+            node=node.next;
         }
-        return temp;
+        
+        return node;
     }
     
-    static Node reverse(Node head){
-        Node prev=null,current=head,next=null;
+    Node reverse(Node node){
+        if(node==null || node.next==null)
+            return node;
         
-        while(current!=null){
-            next=current.next;
-            current.next=prev;
-            prev=current;
-            current=next;
-        }
+        Node newHead = reverse(node.next);
         
-        return prev;
+        node.next.next=node;
+        node.next = null;
+        
+        return newHead;
     }
 }
