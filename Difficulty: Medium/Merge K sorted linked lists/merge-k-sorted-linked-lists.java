bf69105bf18020517010785f4d17d5b6cff1906f
Harshwardhan Shambharkar <149169405+Harshwardhan75@users.noreply.github.com>
2025-09-07 12:22:12 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Merge K sorted linked lists/merge-k-sorted-linked-lists.java
+++ Difficulty: Medium/Merge K sorted linked lists/merge-k-sorted-linked-lists.java
@@ -1,6 +1,4 @@
-//{ Driver Code Starts
-import java.util.*;
-
+/*
 class Node {
     int data;
     Node next;
@@ -10,124 +8,41 @@ class Node {
         next = null;
     }
 }
-
-public class Main {
-    /* Function to print nodes in a given linked list */
-    static void printList(Node node) {
-        while (node != null) {
-            System.out.print(node.data + " ");
-            node = node.next;
-        }
-        System.out.println();
-    }
-
-    // Driver program to test the above functions
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int t = Integer.parseInt(scanner.nextLine());
-
-        while (t-- > 0) {
-            int n = Integer.parseInt(scanner.nextLine());
-            List<Node> lists = new ArrayList<>();
-            for (int i = 0; i < n; i++) {
-                String line = scanner.nextLine();
-                String[] values = line.split(" ");
-                Node head = null, temp = null;
-
-                for (String value : values) {
-                    Node newNode = new Node(Integer.parseInt(value));
-                    if (head == null) {
-                        head = newNode;
-                        temp = head;
-                    } else {
-                        temp.next = newNode;
-                        temp = temp.next;
-                    }
-                }
-
-                lists.add(head);
-            }
-
-            Solution sol = new Solution();
-            Node head = sol.mergeKLists(lists);
-
-            printList(head);
-            System.out.println("~");
-        }
-        scanner.close();
-    }
-}
-
-// } Driver Code Ends
-
-
-// User function Template for Java
-/*class Node
-{
-    int data;
-    Node next;
-
-    Node(int key)
-    {
-        data = key;
-        next = null;
-    }
-}
 */
 
-// arr is an array of Nodes of the heads of linked lists
-
 class Solution {
-    // Function to merge K sorted linked list.
+    Node mergeKLists(Node[] arr) {
+        // code here
+        return merge(0,arr);
+    }
     
-    Node merge(Node one,Node two){
-        Node head=new Node(-1);
-        Node temp=head;
+    Node merge(int index,Node[] arr){
+        if(index==arr.length-1){
+            return arr[index];
+        }
         
-        while(one!=null && two!=null){
-            if(one.data<=two.data){
-                temp.next=one;
-                one=one.next;
+        return merge(arr[index],merge(index+1,arr));
+    }
+    
+    Node merge(Node a,Node b){
+        Node head = new Node(-1);
+        Node temp = head;
+        
+        while(a!=null && b!=null){
+            if(a.data<=b.data){
+                temp.next=a;
+                a=a.next;
             }
             else{
-                temp.next=two;
-                two=two.next;
+                temp.next=b;
+                b=b.next;
             }
+            
             temp=temp.next;
         }
         
-        if(one!=null)   temp.next=one;
-        if(two!=null)   temp.next=two;
-        
-        return head.next;
-    }
-    
-    Node UsingRecursiveCalls(List<Node> arr,int index){
-        if(index==arr.size())
-            return null;
-        
-        return merge(arr.get(index),UsingRecursiveCalls(arr,index+1));
-    }
-    
-    Node mergeKLists(List<Node> arr) {
-        // Add your code here.
-        // return UsingQueue(arr);
-        return UsingRecursiveCalls(arr,0);
-    }
-    
-    Node UsingQueue(List<Node> arr){
-        Node head=new Node(-1);
-        Node temp=head;
-        PriorityQueue<Node> pq=new PriorityQueue<>((a,b)->a.data-b.data);
-        for(Node n: arr)    pq.offer(n);
-        
-        while(!pq.isEmpty()){
-            Node n=pq.poll();
-            temp.next=n;
-            temp=temp.next;
-            n=n.next;
-            if(n!=null) pq.offer(n);
-        }
+        if(a!=null) temp.next=a;
+        if(b!=null) temp.next=b;
         
         return head.next;
     }
