f7dbd5f9a9e3606fd82f0d61fc9d7669564fd42c
Harshwardhan Shambharkar <149169405+Harshwardhan75@users.noreply.github.com>
2025-09-11 19:40:51 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum Jumps/minimum-jumps.java
+++ Difficulty: Medium/Minimum Jumps/minimum-jumps.java
@@ -1,62 +1,23 @@
-//{ Driver Code Starts
-// Initial Template for Java
-
-import java.io.*;
-import java.util.*;
-
-public class Main {
-
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t;
-        t = Integer.parseInt(br.readLine());
-        while (t-- > 0) {
-            String line = br.readLine();
-            String[] tokens = line.split(" ");
-
-            // Create an ArrayList to store the integers
-            ArrayList<Integer> array = new ArrayList<>();
-
-            // Parse the tokens into integers and add to the array
-            for (String token : tokens) {
-                array.add(Integer.parseInt(token));
-            }
-
-            int[] arr = new int[array.size()];
-            int idx = 0;
-            for (int i : array) arr[idx++] = i;
-
-            System.out.println(new Solution().minJumps(arr));
-            System.out.println("~");
-        }
-    }
-}
-
-// } Driver Code Ends
-
-
 class Solution {
-    static int minJumps(int[] arr) {
+    public int minJumps(int[] arr) {
         // code here
-        int n=arr.length;
-        int left = 0,right = 0;
-        int jump =0;
+        int jump = 0;
+        int left = 0, right = 0;
         
-        while(right<n-1){
-            int max = 0;
-            
-            if(left>right)  return -1;
+        while(right<arr.length-1){
+            int far = 0;
             
             for(int i=left;i<=right;i++){
-                max=Math.max(max,i+arr[i]);
+                far = Math.max(far,i+arr[i]);
             }
             
-            left = right + 1;
-            right = max;
             jump++;
+            left = right+1;
+            if(right==far)
+                return -1;
+            right = Math.min(arr.length-1,far);
         }
         
         return jump;
     }
-    
 }
