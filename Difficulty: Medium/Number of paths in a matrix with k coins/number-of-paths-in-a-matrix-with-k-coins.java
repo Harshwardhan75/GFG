d0484a23656858f84b48a5a7bd041cae2b92bb62
Harshwardhan Shambharkar <149169405+Harshwardhan75@users.noreply.github.com>
2025-11-08 14:33:32 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Number of paths in a matrix with k coins/number-of-paths-in-a-matrix-with-k-coins.java
+++ Difficulty: Medium/Number of paths in a matrix with k coins/number-of-paths-in-a-matrix-with-k-coins.java
@@ -1,22 +1,30 @@
-// User function Template for Java
-
 class Solution {
-    long numberOfPath(int n, int k, int[][] arr) {
+    int n, m;
+    int[][][] dp;
+    public int numberOfPath(int[][] mat, int k) {
         // code here
-        int[][] dp=new int[n][n];
-        return solve(arr,n-1,n-1,k);
+        n = mat.length;
+        m = mat[0].length;
+        
+        dp = new int[n][m][k+1];
+        for(int[][] i: dp)  for(int[] j: i) Arrays.fill(j,-1);
+        
+        return solve(mat,0,0,k);
     }
     
-    long solve(int[][] arr,int row,int col,int k){
-        if(row==0 && col==0)
-            return arr[row][col]==k?1:0;
-        
-        if(row<0 || col<0)
+    int solve(int[][] arr,int r,int c,int sum){
+        if(sum<0 || r>=n || c>=m)
             return 0;
         
-        long up=arr[row][col]<=k?solve(arr,row-1,col,k-arr[row][col]):0;
-        long down=arr[row][col]<=k?solve(arr,row,col-1,k-arr[row][col]):0;
+        if(r==n-1 && c==m-1)
+            return sum-arr[r][c] == 0? 1: 0; 
         
-        return up+down;
+        if(dp[r][c][sum]!=-1)
+            return dp[r][c][sum];
+        
+        int down = solve(arr,r+1,c,sum - arr[r][c]);
+        int right = solve(arr,r,c+1,sum - arr[r][c]);
+        
+        return dp[r][c][sum] = down + right;
     }
 }
