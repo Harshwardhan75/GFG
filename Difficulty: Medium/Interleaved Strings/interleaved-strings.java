2fdda1f62fb4b5b174aefb345c6fa7c2f2360a8c
Harshwardhan Shambharkar <149169405+Harshwardhan75@users.noreply.github.com>
2025-11-13 13:17:07 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Interleaved Strings/interleaved-strings.java
+++ Difficulty: Medium/Interleaved Strings/interleaved-strings.java
@@ -14,10 +14,16 @@ class Solution {
             return false;
         dp = new Boolean[n1+1][n2+1];
         
-        return solve(s1,0,s2,0,s3,0);
+        return solve(s1,0,s2,0,s3);
     }
     
-    boolean solve(String s1,int i,String s2,int j,String s3,int k){
+    
+    // Why k is not used for memoization???
+    // Because K is a derived value and can be derieved as a sum of i,j so 
+    // no need to memoize k as it will just explode your states by 600 times
+    boolean solve(String s1,int i,String s2,int j,String s3){
+        int k = i + j;
+        
         if(dp[i][j]!=null)   return dp[i][j];
         
         if(i==n1){
@@ -42,14 +48,14 @@ class Solution {
         boolean status = false;
         
         if(s1.charAt(i)==s3.charAt(k)){
-            status |= solve(s1,i+1,s2,j,s3,k+1);
+            status |= solve(s1,i+1,s2,j,s3);
         }
         
         if(status)
             return dp[i][j] = status;
         
         if(s2.charAt(j)==s3.charAt(k)){
-            status |= solve(s1,i,s2,j+1,s3,k+1);
+            status |= solve(s1,i,s2,j+1,s3);
         }
         
         return dp[i][j] = status;
