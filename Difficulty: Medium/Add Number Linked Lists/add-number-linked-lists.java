21991f7c0655c46a8cec8f40f9e9aa53d2b358c9
Harshwardhan Shambharkar <149169405+Harshwardhan75@users.noreply.github.com>
2025-12-30 12:07:47 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Add Number Linked Lists/add-number-linked-lists.java
+++ Difficulty: Medium/Add Number Linked Lists/add-number-linked-lists.java
@@ -1,13 +1,4 @@
-//{ Driver Code Starts
-// driver
-
-import java.io.*;
-import java.util.*;
-
-
-// } Driver Code Ends
-/* node for linked list
-
+/*
 class Node {
     int data;
     Node next;
@@ -17,118 +8,54 @@ class Node {
         next = null;
     }
 }
-
 */
 
 class Solution {
-    static Node reverse(Node head){
-        Node prev=null,curr=head,next=null;
-        while(curr!=null){
-            next=curr.next;
-            curr.next=prev;
-            prev=curr;
-            curr=next;
-        }
+    
+    Node reverse(Node root){
+        if(root==null || root.next == null)
+            return root;
         
-        return prev;
-    }
-    static Node addTwoLists(Node num1, Node num2) {
-        // code here
-        
-        Node one=reverse(num1);
-        Node two=reverse(num2);
-        
-        int carry=0;
-        Node dummy=new Node(-1);
-        Node temp=dummy;
-        while(one!=null || two!=null){
-            int sum=0;
-            sum+=carry;
-            if(one!=null){
-                sum+=one.data;
-                one=one.next;
-            }
-            
-            if(two!=null){
-                sum+=two.data;
-                two=two.next;
-            }
-            
-            carry=sum/10;
-            temp.next=new Node(sum%10);
-            temp=temp.next;
-        }
-        
-        Node newHead = reverse(dummy.next);
-        if(carry==1){
-            Node newNode = new Node(carry);
-            newNode.next=newHead;
-            newHead=newNode;
-        }
-        
-        while(newHead != null && newHead.data==0)
-            newHead=newHead.next;
+        Node newHead = reverse(root.next);
+        root.next.next = root;
+        root.next = null;
         return newHead;
     }
-}
-
-
-//{ Driver Code Starts.
-
-class Node {
-    int data;
-    Node next;
-
-    Node(int d) {
-        data = d;
-        next = null;
-    }
-}
-
-class GfG {
-
-    static void printList(Node n) {
-        while (n != null) {
-            System.out.print(n.data + " ");
-            n = n.next;
+    
+    public Node addTwoLists(Node head1, Node head2) {
+        // code here
+        head1 = reverse(head1);
+        head2 = reverse(head2);
+        
+        int carry = 0;
+        
+        Node dummy = new Node(-1);
+        Node temp = dummy;
+        
+        while(head1!=null || head2!=null){
+            int sum = (head1!=null?head1.data:0) + 
+                        (head2!=null?head2.data:0) + 
+                        carry;
+            
+            carry = sum / 10;
+            temp.next = new Node(sum%10);
+            
+            if(head1!=null)
+                head1=head1.next;
+            if(head2!=null)
+                head2=head2.next;
+            temp = temp.next;
         }
-        System.out.println();
-    }
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(read.readLine());
-        while (T-- > 0) {
-
-            String str[] = read.readLine().trim().split(" ");
-            int n = str.length;
-
-            Node num1 = new Node(Integer.parseInt(str[0]));
-            Node tail = num1;
-            for (int i = 1; i < n; i++) {
-                int val = Integer.parseInt(str[i]);
-                tail.next = new Node(val);
-                tail = tail.next;
-            }
-
-            String str2[] = read.readLine().trim().split(" ");
-            int m = str2.length;
-
-            Node num2 = new Node(Integer.parseInt(str2[0]));
-            tail = num2;
-            for (int i = 1; i < m; i++) {
-
-                tail.next = new Node(Integer.parseInt(str2[i]));
-                tail = tail.next;
-            }
-
-            Solution g = new Solution();
-            Node res = g.addTwoLists(num1, num2);
-            printList(res);
-
-            System.out.println("~");
+        
+        if(carry==1){
+            temp.next = new Node(carry);
         }
+        
+        dummy = reverse(dummy.next);
+        
+        while(dummy!=null && dummy.data == 0)
+            dummy = dummy.next;
+        
+        return dummy;
     }
 }
-
-// } Driver Code Ends
