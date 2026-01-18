19f68f4f26f4677a53fb51abf5d3b9d34a878356
Harshwardhan Shambharkar <149169405+Harshwardhan75@users.noreply.github.com>
2026-01-18 15:33:07 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Next element with greater frequency/next-element-with-greater-frequency.java
+++ Difficulty: Medium/Next element with greater frequency/next-element-with-greater-frequency.java
@@ -1,22 +1,22 @@
 class Solution {
-    public ArrayList<Integer> findGreater(int[] arr) {
+
+    public ArrayList<Integer> nextFreqGreater(int[] arr) {
         // code here
-        Map<Integer,Integer> map=new HashMap<>();
-        for(int i: arr) map.compute(i,(k,v)->v==null?1:v+1);
-    
-        Stack<Integer> st=new Stack<>();
-        int n=arr.length;
         ArrayList<Integer> result = new ArrayList<>();
         
-        for(int i=n-1;i>=0;i--){
-            while(!st.isEmpty() && map.get(st.peek())<=map.get(arr[i]))
-                st.pop();
-            
-            result.add(
-                st.isEmpty()?-1:st.peek()
-            );
+        int n = arr.length;
+        int[] freq = new int[100001];
+        for(int i: arr)
+            freq[i]++;
         
-            st.push(arr[i]);
+        Stack<Integer> st = new Stack<>();
+        for(int i=n-1;i>=0;i--){
+            while(!st.isEmpty() && freq[arr[st.peek()]]<=freq[arr[i]]){
+                st.pop();
+            }
+            
+            result.add(st.isEmpty()?-1:arr[st.peek()]);
+            st.push(i);
         }
         
         Collections.reverse(result);
