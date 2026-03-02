1ee3d97023dc46aa55868d1ff3b5eeb2004b6fdb
Harshwardhan Shambharkar <149169405+Harshwardhan75@users.noreply.github.com>
2026-03-02 20:47:53 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Trapping Rain Water/trapping-rain-water.java
+++ Difficulty: Hard/Trapping Rain Water/trapping-rain-water.java
@@ -1,45 +1,27 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.util.*;
-
-class Sorting {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine());
-        for (int g = 0; g < t; g++) {
-            String[] str = (br.readLine()).trim().split(" ");
-            int arr[] = new int[str.length];
-            for (int i = 0; i < str.length; i++) arr[i] = Integer.parseInt(str[i]);
-            System.out.println(new Solution().maxWater(arr));
-            System.out.println("~");
-        }
-    }
-}
-// } Driver Code Ends
-
-
-
-
 class Solution {
     public int maxWater(int arr[]) {
         // code here
-        int leftmax=-1,rightmax=-1;
-        int low=0,high=arr.length-1;
-        int count=0;
-        while(low<=high){
-            if(arr[low]<arr[high]){
-                if(leftmax>arr[low])
-                    count+=leftmax-arr[low];
+        int leftMax = 0, rightMax = 0;
+        
+        int left = 0 , right = arr.length-1;
+        int count = 0;
+        
+        while(left<right){
+            if(arr[left]<arr[right]){
+                if(leftMax>arr[left])
+                    count += leftMax - arr[left];
                 else
-                    leftmax=arr[low];
-                low++;
+                    leftMax = arr[left];
+                    
+                left++;
             }
             else{
-                if(rightmax>arr[high])
-                    count+=rightmax-arr[high];
+                if(rightMax>arr[right])
+                    count += rightMax - arr[right];
                 else
-                    rightmax=arr[high];
-                    high--;
+                    rightMax = arr[right];
+                
+                right--;
             }
         }
         
