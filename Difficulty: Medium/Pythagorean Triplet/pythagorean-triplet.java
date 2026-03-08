b8a736377decac7da63c13b70cb8701e7f3d67b1
Harshwardhan Shambharkar <149169405+Harshwardhan75@users.noreply.github.com>
2026-03-08 09:03:08 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Pythagorean Triplet/pythagorean-triplet.java
+++ Difficulty: Medium/Pythagorean Triplet/pythagorean-triplet.java
@@ -1,27 +1,28 @@
 class Solution {
     boolean pythagoreanTriplet(int[] arr) {
         // code here
-        boolean[] freq=new boolean[1001];
-        
+        int[] freq = new int[1001];
         for(int i: arr)
-            freq[i]=true;
+            freq[i] = 1;
         
         for(int i=1;i<freq.length;i++){
-            if(!freq[i]) continue;
+            if(freq[i]==0)  continue;
+        
             for(int j=i+1;j<freq.length;j++){
-                if(!freq[j]) continue;
-                int square=i*i+j*j;
+                if(freq[j]==0)  continue;
                 
-                double x = Math.sqrt((double)square);
-                if(x%1.0==0.00){
-                    int value = (int)x;
+                int square = (i*i)+(j*j);
+                
+                double value = Math.sqrt((double)square);
+                
+                if(value%1.00==0.00){
+                    int val = (int)value;
                     
-                    if(value<=1000 && freq[value])
+                    if(freq[val]>0)
                         return true;
                 }
             }
         }
-        
         return false;
     }
 }
